5f3c2290e25ec5ebe19a21029b516d2c34caf94a
developer-fardin786 <developerfardin2.0@gmail.com>
2023-08-30 14:51:20 +0530

    added prime num code

---
+++ git_assignment/prime.js
@@ -0,0 +1,13 @@
+let num=34;
+let factor=0;
+for(let i=0;i<=num;i++){
+    if(num%i==0){
+        factor++
+    }
+}
+
+if(factor==1){
+    console.log("prime num")
+}else{
+    console.log("not a prime num")
+}
